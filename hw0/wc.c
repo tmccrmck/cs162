9b41513e072a53aae1a61b02f96a79e5a269b192
Trent <tjmcc01@gmail.com>
2015-01-29 19:56:58 +0000

    Trigger autograder.

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <ctype.h>
-
 void wc(FILE *ofile, FILE *infile, char *inname) {
+    int lines = 0; 
+    int words = 0; 
+    int bytes = 0;
+    char c;
+    char lastline = ' ';
+    while(c = fgetc(infile), !feof(infile)){
+	bytes += 1;
+	if(c == '\n'){
+	    lines += 1;
+	}
+	if(isspace(c) && !isspace(lastline)){
+	    words += 1;
+	}
+
+	lastline = c;
+    }
+	
+    fprintf(ofile, " %d %d %d %s\n", lines, words, bytes, inname);
 }
 
 int main (int argc, char *argv[]) {
+    FILE * input = fopen(argv[1], "r");
+
+    if(argc == 3){
+	FILE * output = fopen(argv[2], "w");
+	wc(output, input, argv[1]);
+	fclose(output);
+    } else if(argc == 2){
+	wc(stdout, input, argv[1]);
+    } else if(argc == 1){
+	wc(stdout, stdin, "test");
+    }
+    fclose(input);
     return 0;
 }
