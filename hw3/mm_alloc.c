60ccb065bb5bed8594e4758c0f76d71b778b1b01
Trent <tjmcc01@gmail.com>
2015-03-29 05:51:44 +0000

    test12

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -10,7 +10,80 @@
 #include <stdlib.h>
 
 /* Your final implementation should comment out this macro. */
-#define MM_USE_STUBS
+/* #define MM_USE_STUBS */
+#define align4(x) (((((x)-1)>>2)<<2)+4)
+void *base = NULL;
+
+s_block_ptr find_block(s_block_ptr *last, size_t size){
+  s_block_ptr b = base;
+	while (b && !(b->free && b->size >= size)){
+    *last = b;
+		b = b->next;
+	}
+	return b;
+}
+void split_block (s_block_ptr b, size_t s){
+  s_block_ptr new;
+	new = (s_block_ptr)(b->data + s);
+	new->size = b->size - s - BLOCK_SIZE ;
+	new->next = b->next;
+	new->prev = b;
+  new->free = 1;
+	new->ptr = new->data;
+
+	b->size = s;
+	if(new->next)
+		new->next->prev = new;
+}
+
+s_block_ptr extend_heap (s_block_ptr last, size_t s){
+  s_block_ptr b;
+	int sb;
+	b = sbrk(0);
+	sb = (int) sbrk(BLOCK_SIZE + s);
+	if (sb < 0)
+		return NULL;
+	b->size = s;
+	b->next = NULL;
+	b->prev = last;
+	b->ptr = b->data;
+	if(last)
+		last->next = b;
+	b->free = 0;
+	return b;
+}
+
+s_block_ptr fusion(s_block_ptr b){
+  if(b->next && b->next->free){
+    b->size += BLOCK_SIZE + b->next->size;
+		b->next = b->next->next;
+		if (b->next)
+			b->next->prev = b;
+  }
+}
+
+s_block_ptr get_block(void *p){
+  char *tmp;
+	tmp = p;
+	return (p = tmp -= BLOCK_SIZE);
+}
+
+int valid_addr(void *p){
+  if(base){
+		if(p > base && p < sbrk(0)){
+			return (p == ( get_block (p))->ptr);
+		}
+	}
+	return 0;
+}
+void copy_block(s_block_ptr src, s_block_ptr dst){
+  int *sdata, *ddata;
+	size_t i;
+	sdata = src->ptr;
+	ddata = dst->ptr;
+	for (i=0; i*4<src ->size && i*4<dst ->size; i++)
+		ddata[i] = sdata[i];
+}
 
 void* mm_malloc(size_t size)
 {
@@ -20,6 +93,27 @@ void* mm_malloc(size_t size)
 #else
 #error Not implemented.
 #endif
+	s_block_ptr b, last;
+	size_t s = align4(size);
+	if(base){
+    last = base;
+		b = find_block(&last, s);
+		if(b){
+      if ((b->size - s) >= ( BLOCK_SIZE + 4))
+				split_block(b,s);
+			b->free = 0;
+		} else{
+      b = extend_heap(last,s);
+			if (!b)
+				return NULL;
+		}
+	} else{
+    b = extend_heap(NULL, s);
+		if(!b)
+			return NULL;
+		base = b;
+	}
+	return b->data;
 }
 
 void* mm_realloc(void* ptr, size_t size)
@@ -29,6 +123,7 @@ void* mm_realloc(void* ptr, size_t size)
 #else
 #error Not implemented.
 #endif
+		return 0;
 }
 
 void mm_free(void* ptr)
@@ -38,4 +133,22 @@ void mm_free(void* ptr)
 #else
 #error Not implemented.
 #endif
+  s_block_ptr b;
+  if(valid_addr(ptr)){
+    b = get_block(ptr);
+		b->free = 1;
+    if (b->prev && b->prev ->free)
+			b = fusion(b->prev);
+
+    if (b->next)
+			fusion(b);
+		else{
+      if(b->prev)
+				b->prev->next = NULL;
+			else
+				base = NULL;
+			brk(b);
+		}
+	}
 }
+
